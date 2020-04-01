393fce9a29820f94ef2f338f5f1d4a401636ad28
Anders Qvistgaard Højer <ande711e@edu.easj.dk>
2020-04-01 12:51:09 +0200

    ww

---
+++ Controllers/GuestController.cs
@@ -0,0 +1,35 @@
+﻿using ModelKlasser;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Controllers
+{
+    class GuestController : IManageGuest
+    {
+        public bool CreateGuest(Guest guest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Guest DeleteGuest(int guestNr)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Guest> GetAllGuest()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Guest GetGuestFromId(int guestNr)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateGuest(Guest guest, int guestNr)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
